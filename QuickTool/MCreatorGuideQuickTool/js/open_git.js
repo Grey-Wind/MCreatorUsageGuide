2c4994ffa4196f84ac063225cfc3903ea4940151
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-15 18:31:57 +0800

    Create open_git.js

---
+++ QuickTool/MCreatorGuideQuickTool/js/open_git.js
@@ -0,0 +1,6 @@
+function compressed_grass_download() {
+    window.open("https://github.com/Grey-Wind/MCreatorUsageGuide", "_blank");
+}
+function compressed_grass_download() {
+    window.open("https://gitee.com/at-studio/MCreatorUsageGuide", "_blank");
+}
